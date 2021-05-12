5f2158429183974178afbdd95306b26ca11383dc
neptunium21 <neptun7@inbox.lv>
2021-05-12 03:10:04 +0300

    change setting.py - add .apps to DepositConfig

--- deposit_project/settings.py
+++ deposit_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'deposit.DepositConfig',
+    'deposit.apps.DepositConfig',
 ]
 
 MIDDLEWARE = [
